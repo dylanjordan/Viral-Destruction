369ab0e11d18ff4949b5e4b605752551b26f9591
Dylan Jordan <dylanpeterjordan@gmail.com>
2022-11-21 10:26:59 -0500

    folder organizing

---
+++ Assets/[Scripts]/Player/FirstPersonController.cs
@@ -0,0 +1,431 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public bool CanMove { get; private set; } = true;
+    private bool IsSprinting => canSprint && input.GetSprint();
+    private bool ShouldJump => input.GetJumped() && characterController.isGrounded;
+    private bool ShouldCrouch => input.GetCrouch() && !duringCrouchAnim && characterController.isGrounded;
+
+    [Header("Functional Options")]
+    [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canJump = true;
+    [SerializeField] private bool canCrouch = true;
+    [SerializeField] private bool canUseHeadbob = true;
+    [SerializeField] private bool willSlideOnSlopes = true;
+    [SerializeField] private bool canZoom = true;
+    [SerializeField] private bool canInteract = true;
+    [SerializeField] private bool useFootsteps = true;
+
+    [Header("Movement Parameters")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintSpeed = 6.0f;
+    [SerializeField] private float crouchSpeed = 1.5f;
+    [SerializeField] private float slopeSpeed = 8.0f;
+
+    [Header("Footstep Parameters")]
+    [SerializeField] private float baseStepSpeed = 0.5f;
+    [SerializeField] private float crouchStepMultiplier = 1.5f;
+    [SerializeField] private float sprintStepMutliplier = 0.6f;
+    [SerializeField] private AudioSource footstepAudioSource = default;
+    [SerializeField] private AudioClip[] woodClips= default;
+    [SerializeField] private AudioClip[] metalClips= default;
+    [SerializeField] private AudioClip[] grassClips= default;
+    private float footstepTimer = 0;
+    private float GetCurrentOffset => isCrouching ? baseStepSpeed * crouchStepMultiplier : IsSprinting ? baseStepSpeed * sprintStepMutliplier : baseStepSpeed;
+
+    [Header("Look Parameters")]
+    [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f;
+    [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
+    [SerializeField, Range(1, 180)] private float upperLookLimit = 80.0f;
+    [SerializeField, Range(1, 180)] private float lowerLookLimit = 80.0f;
+
+    [Header("Health Parameters")]
+    [SerializeField] private float maxHealth = 100;
+    [SerializeField] private float timeBeforeRegenStarts = 3;
+    [SerializeField] private float healthValueIncrement = 1;
+    [SerializeField] private float healthTimeIncrement = 0.1f;
+    private float currentHealth;
+    private Coroutine regeneratingHealth;
+    public static Action<float> OnTakeDamage;
+    public static Action<float> OnDamage;
+    public static Action<float> OnHeal;
+
+    [Header("Jumping Parameters")]
+    [SerializeField] private float jumpForce = 8.0f;
+    [SerializeField] private float gravity = 30.0f;
+
+    [Header("Crouch Parameters")]
+    [SerializeField] private float crouchHeight = 0.5f;
+    [SerializeField] private float standingHeight = 2.0f;
+    [SerializeField] private float timeToCrouch = 0.25f;
+    [SerializeField] private Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
+    [SerializeField] private Vector3 standingCenter = Vector3.zero;
+    private bool isCrouching;
+    private bool duringCrouchAnim;
+
+    [Header("Headbob Parameters")]
+    [SerializeField] private float walkBobSpeed = 14f;
+    [SerializeField] private float walkBobAmount = 0.05f;
+    [SerializeField] private float sprintBobSpeed = 18f;
+    [SerializeField] private float sprintBobAmount = 0.1f;
+    [SerializeField] private float crouchBobSpeed = 8f;
+    [SerializeField] private float crouchBobAmount = 0.025f;
+    private float defaultYPos = 0;
+    private float timer;
+
+    [Header("Zoom Parameters")]
+    [SerializeField] private float timeToZoom = 0.3f;
+    [SerializeField] private float zoomFOV = 30f;
+    private float defaultFOV;
+    private Coroutine zoomRoutine;
+
+    // SLIDING PARAMETERS
+    private Vector3 hitPointNormal;
+
+    private bool IsSliding
+    {
+        get
+        {
+            if (characterController.isGrounded && Physics.Raycast(transform.position, Vector3.down, out RaycastHit slopeHit, 2f))
+            {
+                hitPointNormal = slopeHit.normal;
+                return Vector3.Angle(hitPointNormal, Vector3.up) > characterController.slopeLimit;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    [Header("Interaction")]
+    [SerializeField] private Vector3 interactionRayPoint = default;
+    [SerializeField] private float interactionDistance = default;
+    [SerializeField] private LayerMask interactionLayer = default;
+    private Interactable currentInteractable;
+
+    private Camera playerCamera;
+    private CharacterController characterController;
+    private InputManager input;
+
+    private Vector3 moveDirection;
+    private Vector2 currentInput;
+
+    private float rotationX = 0;
+
+    private void OnEnable()
+    {
+        OnTakeDamage += ApplyDamage;
+    }
+
+    private void OnDisable()
+    {
+        OnTakeDamage -= ApplyDamage;
+    }
+
+    void Awake()
+    {
+        playerCamera = GetComponentInChildren<Camera>();
+        characterController = GetComponent<CharacterController>();
+        defaultYPos = playerCamera.transform.localPosition.y;
+        defaultFOV = playerCamera.fieldOfView;
+        currentHealth = maxHealth;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Start()
+    {
+        input = InputManager.Instance;
+    }
+
+    void Update()
+    {
+        if (CanMove)
+        {
+            HandleMovementInput();
+            HandleMouseLook();
+
+            if (canJump)
+                HandleJump();
+
+            if (canCrouch)
+                HandleCrouch();
+
+            if (canUseHeadbob)
+                HandleHeadbob();
+
+            if (canZoom)
+                HandleZoom();
+
+            if (useFootsteps)
+                HandleFootsteps();
+
+            if (canInteract)
+            {
+                HandleInteractionCheck();
+                HandleInteractionInput();
+            }
+
+            ApplyFinalMovements();
+        }
+    }
+
+    private void HandleMovementInput()
+    {
+        // Get movement input & speed
+        currentInput = new Vector2((isCrouching ? crouchSpeed : IsSprinting ? sprintSpeed : walkSpeed) * input.GetPlayerMovement().y, (isCrouching ? crouchSpeed : IsSprinting ? sprintSpeed : walkSpeed) * input.GetPlayerMovement().x);
+
+        // Store movement values
+        float moveDirectionY = moveDirection.y;
+        moveDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
+        moveDirection.y = moveDirectionY;
+    }
+
+    private void HandleMouseLook()
+    {
+        //Look up and down
+        rotationX -= input.GetMouseMovement().y * lookSpeedY * Time.deltaTime;
+        rotationX = Mathf.Clamp(rotationX, -upperLookLimit, lowerLookLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+
+        // Look left and right
+        transform.rotation *= Quaternion.Euler(0, input.GetMouseMovement().x * lookSpeedX * Time.deltaTime, 0);
+    }
+
+    private void HandleJump()
+    {
+        if (ShouldJump)
+            moveDirection.y = jumpForce;
+    }
+
+    private void HandleCrouch()
+    {
+        if (ShouldCrouch)
+            StartCoroutine(CrouchStand());
+    }
+
+    private void HandleHeadbob()
+    {
+        // Check if character is grounded
+        if (!characterController.isGrounded)
+            return;
+
+        // Check if player is moving
+        if(Mathf.Abs(moveDirection.x) > 0.1f || Mathf.Abs(moveDirection.z) > 0.1f)
+        {
+            // Determine headbob speed & amount : Apply
+            timer += Time.deltaTime * (isCrouching ? crouchBobSpeed : IsSprinting ? sprintBobSpeed : walkBobSpeed);
+            playerCamera.transform.localPosition = new Vector3(
+                playerCamera.transform.localPosition.x,
+                defaultYPos + Mathf.Sin(timer) * (isCrouching ? crouchBobAmount : IsSprinting ? sprintBobAmount : walkBobAmount),
+                playerCamera.transform.localPosition.z);
+        }
+    }
+
+    //This should be new input system
+    private void HandleZoom()
+    {
+        if (input.GetADSEnable())
+        {
+            if (zoomRoutine != null)
+            {
+                StopCoroutine(zoomRoutine);
+                zoomRoutine = null;
+            }
+            zoomRoutine = StartCoroutine(ToggleZoom(true));
+        }
+        if (input.GetADSDisable())
+        {
+            if (zoomRoutine != null)
+            {
+                StopCoroutine(zoomRoutine);
+                zoomRoutine = null;
+            }
+            zoomRoutine = StartCoroutine(ToggleZoom(false));
+        }
+    }
+
+    private void HandleInteractionCheck()
+    {
+        // Check if raycast is hitting interactable
+        if (Physics.Raycast(playerCamera.ViewportPointToRay(interactionRayPoint), out RaycastHit hit, interactionDistance))
+        {
+            // If on interactable layer and (no interactable object selected or object your looking at isnt the current object stored
+            if (hit.collider.gameObject.layer == 9 && (currentInteractable == null || hit.collider.gameObject.GetInstanceID() != currentInteractable.GetInstanceID()))
+            {
+                // Get new current interactable
+                hit.collider.TryGetComponent(out currentInteractable);
+
+                if (currentInteractable)
+                    currentInteractable.OnFocus();
+            }
+        }
+        // If raycast doesnt find anything
+        else if (currentInteractable)
+        {
+            currentInteractable.OnLoseFocus();
+            currentInteractable = null;
+        }
+    }
+
+    private void HandleInteractionInput()
+    {
+        // Check if looking at interactable & input
+        if (input.GetInteract() && currentInteractable != null && 
+            Physics.Raycast(playerCamera.ViewportPointToRay(interactionRayPoint), out RaycastHit hit, interactionDistance, interactionLayer))
+        {
+            currentInteractable.OnInteract();
+        }
+    }
+
+    private void HandleFootsteps()
+    {
+        if (!characterController.isGrounded)
+            return;
+
+        if (currentInput == Vector2.zero)
+            return;
+
+        footstepTimer -= Time.deltaTime;
+
+        if (footstepTimer <= 0)
+        {
+            if (Physics.Raycast(playerCamera.transform.position, Vector3.down, out RaycastHit hit, 3))
+            {
+                switch (hit.collider.tag)
+                {
+                    case "Footsteps/WOOD":
+                        footstepAudioSource.PlayOneShot(woodClips[UnityEngine.Random.Range(0, woodClips.Length - 1)]);
+                        break;
+                    case "Footsteps/METAL":
+                        footstepAudioSource.PlayOneShot(metalClips[UnityEngine.Random.Range(0, metalClips.Length - 1)]);
+                        break;
+                    case "Footsteps/GRASS":
+                        footstepAudioSource.PlayOneShot(grassClips[UnityEngine.Random.Range(0, grassClips.Length - 1)]);
+                        break;
+                    default:
+                        footstepAudioSource.PlayOneShot(grassClips[UnityEngine.Random.Range(0, grassClips.Length - 1)]);
+                        break;
+                }
+            }
+
+            footstepTimer = GetCurrentOffset;
+        }
+    }
+
+    private void ApplyDamage(float damage)
+    {
+        currentHealth -= damage;
+        OnDamage?.Invoke(currentHealth);
+
+        if (currentHealth <= 0) // Check if players health is 0
+            KillPlayer();
+        else if (regeneratingHealth != null) // Stop healing
+            StopCoroutine(regeneratingHealth);
+
+        regeneratingHealth = StartCoroutine(RegenerateHealth()); // Restart regen
+    }
+    
+    private void KillPlayer()
+    {
+        currentHealth = 0;
+
+        if (regeneratingHealth != null)
+            StopCoroutine(regeneratingHealth);
+
+        print("DEAD");
+    }
+
+    private void ApplyFinalMovements()
+    {
+        // Apply gravity if in air
+        if (!characterController.isGrounded)
+            moveDirection.y -= gravity * Time.deltaTime;
+
+        if (characterController.velocity.y < -1 && characterController.isGrounded)
+            moveDirection.y = 0;
+
+        // Apply slide to movement if on slope
+        if (willSlideOnSlopes && IsSliding)
+            moveDirection += new Vector3(hitPointNormal.x, -hitPointNormal.y, hitPointNormal.z) * slopeSpeed;
+
+        // Move character
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    private IEnumerator CrouchStand()
+    {
+        // Check if object is above character
+        if (isCrouching && Physics.Raycast(playerCamera.transform.position, Vector3.up, 1f))
+            yield break;
+
+        // Start crouchign anim(time)
+        duringCrouchAnim = true;
+
+        // Set height and center variables
+        float timeElapsed = 0;
+        float targetHeight = isCrouching ? standingHeight : crouchHeight;
+        float currentHeight = characterController.height;
+        Vector3 targetCenter = isCrouching ? standingCenter : crouchingCenter;
+        Vector3 currentCenter = characterController.center;
+
+        // Lerp height and center
+        while(timeElapsed < timeToCrouch)
+        {
+            characterController.height = Mathf.Lerp(currentHeight, targetHeight, timeElapsed / timeToCrouch);
+            characterController.center = Vector3.Lerp(currentCenter, targetCenter, timeElapsed / timeToCrouch);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        // Confirm height & center are correct
+        characterController.height = targetHeight;
+        characterController.center = targetCenter;
+
+        // Change crouching bool
+        isCrouching = !isCrouching;
+
+        // End crouching anim(time)
+        duringCrouchAnim = false;
+    }
+
+    private IEnumerator ToggleZoom(bool isEnter)
+    {
+        float targetFOV = isEnter ? zoomFOV : defaultFOV;
+        float startingFOV = playerCamera.fieldOfView;
+        float timeElapsed = 0;
+
+        while(timeElapsed < timeToZoom)
+        {
+            playerCamera.fieldOfView = Mathf.Lerp(startingFOV, targetFOV, timeElapsed / timeToZoom);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        playerCamera.fieldOfView = targetFOV;
+        zoomRoutine = null;
+    }
+
+    private IEnumerator RegenerateHealth()
+    {
+        yield return new WaitForSeconds(timeBeforeRegenStarts);
+        WaitForSeconds timetoWait = new WaitForSeconds(healthTimeIncrement);
+
+        while (currentHealth < maxHealth)
+        {
+            currentHealth += healthValueIncrement;
+
+            if (currentHealth > maxHealth)
+                currentHealth = maxHealth;
+
+            OnHeal?.Invoke(currentHealth);
+            yield return timetoWait;
+        }
+
+        regeneratingHealth = null;
+    }
+}
---
+++ Assets/[Scripts]/Weapons/Gun.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class Gun : MonoBehaviour
+{
+    [Header("Weapon Stats")]
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+
+    [Header("Ammo Parameters")]
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    private int stockAmmo = 0;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
+    [Header("Additional Variables")]
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public Animator animator;
+    public TextMeshProUGUI text;
+
+    private float nextTimeToFire = 0f;
+
+    private InputManager input;
+
+    void Start()
+    {
+        input = InputManager.Instance;
+        currentAmmo = maxAmmo;
+    }
+
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
+
+    void Update()
+    {
+        if (isReloading)
+            return;
+
+        if (stockAmmo > 0 && (currentAmmo <= 0 || input.GetReload()))
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (input.GetShooting() && Time.time >= nextTimeToFire && currentAmmo > 0)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+
+        UpdateAmmoText();
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        currentAmmo--;
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+        }
+    }
+
+    public void AddAmmo(int ammoAmount)
+    {
+        stockAmmo += ammoAmount;
+    }
+
+    private void UpdateAmmoText()
+    {
+        text.text = $"{currentAmmo} / {stockAmmo}";
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - 0.25f);
+
+        animator.SetBool("Reloading", false);
+
+        yield return new WaitForSeconds(0.25f);
+
+        int reloadAmount = maxAmmo - currentAmmo; // How mnay bullets to refill clip
+
+        reloadAmount = (stockAmmo - reloadAmount) >= 0 ? reloadAmount : stockAmmo; // Check if enough bullets in stock
+        currentAmmo += reloadAmount;
+        stockAmmo -= reloadAmount;
+
+        //currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+}
